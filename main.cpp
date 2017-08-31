dcd6d06dbe5e60937492f653d9b98b75d8cea69d
shubhMaheshwari <maheshwarishubh98@gmail.com>
2017-08-31 08:58:08 +0530

    Bug Fixed

--- main.cpp
+++ main.cpp
@@ -23,17 +23,10 @@ signed main() {
 		// prints the PS1
 		b.print_term();
 
-		
+
 		cmd = getInput();
+
 		cmd = remove_padding(cmd); // removing the leading and trailing spaces
-		
-		if(strstr(cmd,"exit") || strstr(cmd,"quit")) { exit(0); break; } // exit shell on quit
-		else if(strcmp(cmd, "pinfo") == 0) {
-			cout<<"PID\t"<<"Name"<<endl;
-			for(auto i: all_proc)
-				cout<<i.first<<"\t"<<i.second.get_name()<<endl;
-			continue;
-		}
 		
 		/* EXECUTION OF PROCESS */
 		exe_cmds(cmd);
