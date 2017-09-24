2bb994b9a074019e24306c10632ba1cf558ed99f
pk13055 <pkrockstar7@gmail.com>
2017-09-24 17:24:15 +0530

    minor changes

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include <bits/stdc++.h>
-
-#include <readline/readline.h>
 #include <readline/history.h>
-#include <unistd.h>
-
 
 #include "classdef.h"
-#include "colormod.h"
 using namespace std;
 
-signed main() {
+signed main(int argc, char** argv) {
 	
 	BaseDetails b;
 	char *cmd = (char*) malloc(COMMAND_LENGTH * sizeof(char));
 	char shell_prompt[10];
-
 
 	// main process execution loop
 	do {
@@ -23,16 +17,13 @@ signed main() {
 		b.update(); // updates to the pwd
 		check_bg(); // checks for background ends
 
-		// cmd = getInput();
-        
         cmd = b.print_term();
 		cmd = remove_padding(cmd); // removing the leading and trailing spaces
 		
 		/* EXECUTION OF PROCESS */
 		exe_cmds(cmd);
-		
+
     	add_history(cmd);
-        
         free(cmd);
 
 	} while(true);
