157055802dc644872848504bc3ba97049453e64d
shubhMaheshwari <maheshwarishubh98@gmail.com>
2017-09-25 16:13:42 +0530

    Added kjob job history

--- main.cpp
+++ main.cpp
@@ -10,12 +10,15 @@ signed main(int argc, char** argv) {
 	char *cmd = (char*) malloc(COMMAND_LENGTH * sizeof(char));
 	char shell_prompt[10];
 
+	read_history("/home/shubh/Assignment/OS/Assignment2/cpp-shell/.shkell_history");
+
+
 	// main process execution loop
 	do {
 		
 		
 		b.update(); // updates to the pwd
-		check_bg(); // checks for background ends
+		// check_bg(); // checks for background ends
 
         cmd = b.print_term();
 		cmd = remove_padding(cmd); // removing the leading and trailing spaces
